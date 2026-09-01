578076306b99159e9ec2fae707bbd6c855147535
n0izn0iz <7917064+n0izn0iz@users.noreply.github.com>
2026-09-01 18:21:55 +0200

    test(engine): add reproduction for component(addr) method access rule
    Signed-off-by: n0izn0iz <7917064+n0izn0iz@users.noreply.github.com>

---
+++ crates/engine/tests/templates/repro_caller/src/lib.rs
@@ -0,0 +1,31 @@
+use tari_template_lib::prelude::*;
+
+/// Caller template: performs cross-component calls into a callee's methods.
+#[template]
+mod caller {
+    use super::*;
+
+    pub struct Caller;
+
+    impl Caller {
+        pub fn new() -> Component<Self> {
+            let access_rules = ComponentAccessRules::new()
+                .method("call_bar", rule!(allow_all))
+                .method("call_ping", rule!(allow_all))
+                .default(rule!(deny_all));
+
+            Component::new(Caller)
+                .with_access_rules(access_rules)
+                .with_owner_rule(OwnerRule::None)
+                .create()
+        }
+
+        pub fn call_bar(&self, callee: ComponentAddress) {
+            ComponentManager::get(callee).invoke("bar", args![]);
+        }
+
+        pub fn call_ping(&self, callee: ComponentAddress) -> u64 {
+            ComponentManager::get(callee).call("ping", args![])
+        }
+    }
+}
